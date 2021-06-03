cd90990f579f86b9c7cbf4b6f7051285311ac9d1
Javier <ejviquin@gmail.com>
2021-06-03 18:57:12 -0500

    Funcion con js para animacion del scroll

---
+++ trab.js
@@ -0,0 +1,16 @@
+let animado = document.querySelectorAll('.t'); // = selector
+function mostrarScroll(){
+    let scrollTop = document.documentElement.scrollTop;
+    
+    for(i=0;i<animado.length;i++){
+        let alturaAnimado = animado[i].offsetTop;
+        if(alturaAnimado + 150 < scrollTop){
+                animado[i].style.opacity = 1; 
+                animado[i].classList.add('haciArriba');  
+        }else{
+            animado[i].style.opacity = 0; 
+            animado[i].classList.remove('haciArriba'); 
+        }
+    }
+}
+addEventListener('scroll', mostrarScroll);
