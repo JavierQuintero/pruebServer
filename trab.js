66a02285452739650d06e3de56ce15a2441a70e7
Javier <ejviquin@gmail.com>
2021-06-07 17:17:05 -0500

    Se modifica la distancia con relacion al scroll en la animacion de la secion 2

--- trab.js
+++ trab.js
@@ -4,7 +4,7 @@ function mostrarScroll(){
     
     for(i=0;i<animado.length;i++){
         let alturaAnimado = animado[i].offsetTop;
-        if(alturaAnimado + 150 < scrollTop){
+        if(alturaAnimado - 450 < scrollTop){
                 animado[i].style.opacity = 1; 
                 animado[i].classList.add('haciArriba');  
         }else{
