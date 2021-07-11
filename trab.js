b161e642e516fbf5b5da01258cc9ebaaee684c48
Javier <ejviquin@gmail.com>
2021-07-11 18:06:48 -0500

    Header en proceso(efecto)

--- trab.js
+++ trab.js
@@ -1,7 +1,7 @@
 let animado = document.querySelectorAll('.t'); // = selector
 function mostrarScroll(){
     let scrollTop = document.documentElement.scrollTop;
-    
+    //modificarHeader()
     for(i=0;i<animado.length;i++){
         let alturaAnimado = animado[i].offsetTop;
         if(alturaAnimado - 650 < scrollTop){
@@ -14,3 +14,15 @@ function mostrarScroll(){
     }
 }
 addEventListener('scroll', mostrarScroll);
+
+//Para efecto del header
+function modificarHeader(){
+    let logo = document.querySelector('.logoE');
+    let item_menu = document.querySelector('.contenedor_menu');
+
+    logo.children[0].style.fontSize = "2.1em";
+    logo.children[1].style.fontWeight = "300";
+    for(let i=0;i>=item_menu.length;i++){
+        item_menu.children[i].style.fontSize = "1.2em";
+    }
+}
