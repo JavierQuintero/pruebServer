bd60bdf8d67f09f8f9e634901666d0abe52288b1
JaviDev <ejviquin@gmail.com>
2025-01-25 23:20:35 -0500

    actualizacion de indentacion e indexacion en HTML v1.0

--- componente/videoPortada.php
+++ componente/videoPortada.php
@@ -1,11 +1,11 @@
 <section class="caja">
-         <div class="textura"></div>
-         <video loop muted preload autoplay>    
-            <source src="./img/Vpexels-1.mp4" type="video/mp4">   
-         </video>
-         
-         <div class="oC">             
-            <img class="myPhoto iP" src="img/Jum.png" alt="Javier Quintero Desarrollador" />      
-            <p class="imgNombre ani">Javier Quintero</p>
-         </div>
-      </section>
+   <div class="textura"></div>
+   <video loop muted preload autoplay>
+      <source src="./img/Vpexels-1.mp4" type="video/mp4">
+   </video>
+
+   <div class="oC">
+      <img class="myPhoto iP" src="img/Jum.png" alt="Javier Quintero Desarrollador" />
+      <p class="imgNombre ani">Javier Quintero</p>
+   </div>
+</section>
