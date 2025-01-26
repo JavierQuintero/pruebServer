ca2ffeba0d21818e7e109c8aa97ac88560755a44
JaviDev <ejviquin@gmail.com>
2025-01-25 23:20:35 -0500

    actualizacion de indentacion e indexacion en HTML

--- componente/footer.php
+++ componente/footer.php
@@ -1,67 +1,72 @@
 <footer>
-       
-      <div class="container-footer-all" id="contacto_Javi">    
-           <div class="container-body">
-               <div class="colum1">
-                   <h1>Mas informacion de la compañia</h1>
 
-                   <p>Esta compañia se dedica al diseño, posicionamiento e implementacion de nuevas tecnologias a todo tipo de sitios web.</p>
-               </div>
+    <div class="container-footer-all" id="contacto_Javi">
+        <div class="container-body">
+            <div class="colum1">
+                <h1>Mas informacion de la compañia</h1>
 
-               <div class="colum2">
-                   <h1>Redes Sociales</h1>
+                <p>Esta compañia se dedica al diseño, posicionamiento e implementacion de nuevas tecnologias a todo tipo
+                    de sitios web.</p>
+            </div>
 
-                   <div class="row">
-                       <img src="icon/facebook.png">
-                       <label><a href="http://www.facebook.com/JavierQuinteroPosada" target="_blank" rel="noopener noreferrer">Siguenos en Facebook</a></label>
-                   </div>
-                   <div class="row">
-                       <img src="icon/whatsapp.png">
-                       <label><a href="https://wa.me/573112688955" target="_blank" rel="noopener noreferrer">Siguenos en Whatsapp</a></label>
-                   </div>
-                   <div class="row">
-                       <img src="icon/github.png">
-                       <label><a href="http://github.com/JavierQuintero" target="_blank" rel="noopener noreferrer">Siguenos en Github</a></label>
-                   </div>
+            <div class="colum2">
+                <h1>Redes Sociales</h1>
 
-               </div>
+                <div class="row">
+                    <img src="icon/facebook.png">
+                    <label><a href="http://www.facebook.com/JavierQuinteroPosada" target="_blank"
+                            rel="noopener noreferrer">Siguenos en Facebook</a></label>
+                </div>
+                <div class="row">
+                    <img src="icon/whatsapp.png">
+                    <label><a href="https://wa.me/573112688955" target="_blank" rel="noopener noreferrer">Siguenos en
+                            Whatsapp</a></label>
+                </div>
+                <div class="row">
+                    <img src="icon/github.png">
+                    <label><a href="http://github.com/JavierQuintero" target="_blank" rel="noopener noreferrer">Siguenos
+                            en Github</a></label>
+                </div>
 
-               <div class="colum3">
+            </div>
 
-                   <h1>Informacion Contactos</h1>
+            <div class="colum3">
 
-                   <div class="row2">
-                       <img src="icon/house.png">
-                       <label>Bogota, Colombia</label>
-                   </div>
+                <h1>Informacion Contactos</h1>
 
-                   <div class="row2">
-                       <img src="icon/smartphone.png">
-                       <label>+57 3112688955</label>
-                   </div>
+                <div class="row2">
+                    <img src="icon/house.png">
+                    <label>Bogota, Colombia</label>
+                </div>
 
-                   <div class="row2">
-                       <img src="icon/contact.png">
-                        <label>Elkindeveloper@gmail.com</label>
-                   </div>
+                <div class="row2">
+                    <img src="icon/smartphone.png">
+                    <label>+57 3112688955</label>
+                </div>
 
-               </div>
+                <div class="row2">
+                    <img src="icon/contact.png">
+                    <label>Elkindeveloper@gmail.com</label>
+                </div>
 
-           </div>
-       
-       </div>
-       
-       <div class="container-footer">
-              <div class="footer">
-                   <div class="copyright">
-                       © 2021 Todos los Derechos Reservados | <a href="">JavierQuintero</a>
-                   </div>
+            </div>
 
-                   <div class="information">
-                       <a href="">Informacion Compañia</a> | <a href="">Privacion y Politica</a> | <a href="">Terminos y Condiciones</a>
-                   </div>
-               </div>
+        </div>
 
-         </div>
-       
-   </footer>
+    </div>
+
+    <div class="container-footer">
+        <div class="footer">
+            <div class="copyright">
+                © 2021 Todos los Derechos Reservados | <a href="">JavierQuintero</a>
+            </div>
+
+            <div class="information">
+                <a href="">Informacion Compañia</a> | <a href="">Privacion y Politica</a> | <a href="">Terminos y
+                    Condiciones</a>
+            </div>
+        </div>
+
+    </div>
+
+</footer>
--- componente/nosotros.php
+++ componente/nosotros.php
@@ -1,16 +1,19 @@
-<section class="caja pre" id="nosotros">      
-         <div class="tarjePre">
-            <h2>Mi empresa</h2>           
-            <p class="t">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi commodi adipisci sint ea numquam! Cum, necessitatibus! Assumenda asperiores facere ipsum.</p>           
-            <p class="t">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi commodi adipisci sint ea numquam! Cum, necessitatibus! Assumenda asperiores facere ipsum.</p>
-            
-            <ol class="t">
-               <li>Selector</li>
-               <li>Selector</li>
-               <li>Selector</li>
-            </ol>
-            
-            <p class="t">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi commodi adipisci sint ea numquam! Cum, necessitatibus! Assumenda asperiores facere ipsum.</p>
-         
-            </div>
-      </section>
+<section class="caja pre" id="nosotros">
+   <div class="tarjePre">
+      <h2>Mi empresa</h2>
+      <p class="t">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi commodi adipisci sint ea numquam! Cum,
+         necessitatibus! Assumenda asperiores facere ipsum.</p>
+      <p class="t">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi commodi adipisci sint ea numquam! Cum,
+         necessitatibus! Assumenda asperiores facere ipsum.</p>
+
+      <ol class="t">
+         <li>Selector</li>
+         <li>Selector</li>
+         <li>Selector</li>
+      </ol>
+
+      <p class="t">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi commodi adipisci sint ea numquam! Cum,
+         necessitatibus! Assumenda asperiores facere ipsum.</p>
+
+   </div>
+</section>
